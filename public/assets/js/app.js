b8492e0e702c889c8df374dcd1ef29c6cc37ccdd
Eyassu <utopiaio@yahoo.com>
2015-02-25 18:17:38 +0300

    booting up for heroku

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -2,14 +2,35 @@
   'use strict';
 
   angular
-    .module('showtime', ['ngRoute', 'ngTouch', 'ngAria', 'ngAnimate', 'ngMaterial', 'moeProgressMaterial'])
+    .module('showtime', ['ngRoute', 'ngTouch', 'ngAria', 'ngAnimate', 'ngMaterial', 'moeVFill'])
     .config(Config)
     .controller('ShowtimeController', ShowtimeController);
 
   Config.$inject = ['$routeProvider', '$locationProvider', '$mdThemingProvider'];
-  ShowtimeController.$inject = ['$http', '$location', 'xhrInProgress'];
+  ShowtimeController.$inject = ['$http', '$mdToast'];
 
   function Config ($routeProvider, $locationProvider, $mdThemingProvider) {
+    $routeProvider
+      .when('/cinema-1', {
+        templateUrl: 'views/cinema-1.html',
+      })
+
+      .when('/cinema-2', {
+        templateUrl: 'views/cinema-2.html',
+      })
+
+      .when('/cinema-3', {
+        templateUrl: 'views/cinema-3.html',
+      })
+
+      .when('/about', {
+        templateUrl: 'views/about.html'
+      })
+
+      .otherwise({
+        redirectTo: '/cinema-1'
+      });
+
     $mdThemingProvider.theme('default')
         .primaryPalette('blue-grey')
         .accentPalette('orange')
@@ -19,13 +40,26 @@
     $locationProvider.html5Mode(true);
   }
 
-  function ShowtimeController ($http, $location, xhrInProgress) {
-    // this makes sure our URL stays nice and clean
-    $location.path('/').replace();
-    // show toast while $http is in progress
-    xhrInProgress.listenToXHR();
+  function ShowtimeController ($http, $mdToast) {
+    var vm = this;
+
+    var itsShowTimeToast = $mdToast.show({
+      template: '<md-toast class="fixed-toast">'+
+                   '<div flex layout="row" layout-align="start center">'+
+                    '<div flex="20"><md-progress-circular md-mode="indeterminate" md-diameter="24"></md-progress-circular></div>'+
+                    '<div flex>it\'s <span class="showtime">SHOWTIME</span>...<small><i>almost</i></small></div>'+
+                  '</div>'+
+                '</md-toast>',
+      hideDelay: false,
+      position: 'bottom left'
+    });
+
     // it's SHOWTIME!
-    $http.get('showtime');
+    $http.get('showtime')
+      .success(function (data) {
+        $mdToast.hide(itsShowTimeToast);
+        vm.list = data;
+      });
   }
 
 })(window.angular);
---
+++ public/assets/js/moe_angular/moe-vfill.js
@@ -0,0 +1,29 @@
+(function (angular) {
+  'use strict';
+
+  angular.module('moeVFill', [])
+    .directive('fillV', function () {
+      return {
+        restrict: 'A',
+        link: function (scope, element, attribute) {
+          var windowHeight = $(window).height() + 'px';
+
+          $(window).resize(function () {
+            windowHeight = $(window).height() + 'px';
+
+            $(element).css({
+              'height': windowHeight,
+              'min-height': windowHeight,
+              'max-height': windowHeight
+            });
+          });
+
+          $(element).css({
+            'height': windowHeight,
+            'min-height': windowHeight,
+            'max-height': windowHeight
+          });
+        }
+      }
+    });
+})(window.angular);
