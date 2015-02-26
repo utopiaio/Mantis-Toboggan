67dcd835b7bde38c3c452ab99990d64417f8f240
Eyassu <utopiaio@yahoo.com>
2015-02-26 15:35:31 +0300

    enabling cache

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,6 +1,14 @@
 (function (angular) {
   'use strict';
 
+  window.addEventListener('load', function (e) {
+    window.applicationCache.addEventListener('updateready', function (e) {
+      if (window.applicationCache.status === window.applicationCache.UPDATEREADY) {
+        window.location.reload();
+      }
+    }, false);
+  }, false);
+
   angular
     .module('showtime', ['ngRoute', 'ngTouch', 'ngAria', 'ngAnimate', 'ngMaterial', 'moeCase', 'moeMoment'])
     .config(Config)
