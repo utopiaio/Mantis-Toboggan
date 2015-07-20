c5524f3cafd0c3f234a8d087f08f7ae3c8860552
Moe Szyslak <moe.duffdude@gmail.com>
2015-07-20 11:57:37 +0300

    compression
    - ~90kb is being returned

--- app.js
+++ app.js
@@ -2,11 +2,12 @@
   'use strict';
 
   var express = require('express');
+  var compression = require('compression');
   var showtime = require('./lib/showtime.6.babel.js');
 
   var app = express();
   app.set('port', process.env.PORT || 8000);
-
+  app.use(compression());
   app.get('/', function(request, response, next) {
     showtime().then(function(data) {
       response.status(200);
