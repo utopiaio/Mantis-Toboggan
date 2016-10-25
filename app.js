b96abadac17fd63dce2be10890cb3b87f83038ac
Moe Szyslak <moe.duffdude@gmail.com>
2016-10-25 06:40:32 +0300

    moedoo ready for Heroku
    - not using lodash...yet

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require('express');
 const compression = require('compression');
 
 const config = require('./config');
-const moedoo = require('./lib/moedoo')({
+const moedoo = require('./lib/moedoo')(process.env.DATABASE_URL || {
   DB_USER: config.DB_USER,
   DB_PASSWORD: config.DB_PASSWORD,
   DB_HOST: config.DB_HOST,
@@ -14,23 +14,39 @@ const moedoo = require('./lib/moedoo')({
 });
 const a = require('./lib/a')(moedoo);
 
-const app = express();
-app.set('port', process.env.PORT || config.APP_PORT);
-app.use(compression());
+moedoo.query(`
+  CREATE TABLE IF NOT EXISTS detail(
+    detail character varying(1024) NOT NULL,
+    video character varying(1024),
+    CONSTRAINT detail_pk PRIMARY KEY (detail)
+  );
 
-// API...
-app.get('/api', (request, response) => {
-  a().then((info) => {
-    response.status(200).json(info).end();
+  CREATE TABLE IF NOT EXISTS omdb(
+    title character varying(128) NOT NULL,
+    detail jsonb,
+    CONSTRAINT movie_pk PRIMARY KEY (title)
+  );`).then(() => {
+    const app = express();
+    app.set('port', process.env.PORT || config.APP_PORT);
+    app.use(compression());
+
+    // API...
+    app.get('/api', (request, response) => {
+      a().then((info) => {
+        response.status(200).json(info).end();
+      });
+    });
+
+    // fall-back handler...
+    app.all('*', (request, response) => {
+      response.status(400).end();
+    });
+
+    const server = http.createServer(app); // creating server which express will piggy back on
+    server.listen(app.get('port'), config.APP_HOST, () => {
+      console.log(`Server running on ${config.APP_HOST}:${config.APP_PORT}...`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
   });
-});
-
-// fall-back handler...
-app.all('*', (request, response) => {
-  response.status(400).end();
-});
-
-const server = http.createServer(app); // creating server which express will piggy back on
-server.listen(app.get('port'), config.APP_HOST, () => {
-  console.log(`Server running on ${config.APP_HOST}:${config.APP_PORT}...`);
-});
