4c80ea7834e9b1dd17b3a44fa5f22be72d4101cc
Moe Szyslak <moe.duffdude@gmail.com>
2016-10-26 06:49:24 +0300

    .app to .showtime
    - id-ing the colors
    - proper (semantically) name

--- app/www/react-app/component/Menu.jsx
+++ app/www/react-app/component/Menu.jsx
@@ -2,7 +2,15 @@ import React from 'react';
 
 function Menu() {
   return (
-    <div className="app-menu">Menu</div>
+    <div className="showtime-menu">
+      <div className="menu-item">
+        <i className="menu-icon">iC</i>
+        <span className="menu-text">Helth Data</span>
+      </div>
+      <div className="menu-item">2</div>
+      <div className="menu-item">3</div>
+      <div className="menu-item">4</div>
+    </div>
   );
 }
 
