16d77774e1d3e71745af42d83935ddeb482d2825
Moe Szyslak <moe.duffdude@gmail.com>
2016-10-27 07:22:31 +0300

    redux on showtime
    - init (lf cost is around ~200ms, maybe do stuff before splash screen is done 🤔)
    - all dispatchers (except poster) are 👍

--- app/www/react-app/component/Header.jsx
+++ app/www/react-app/component/Header.jsx
@@ -1,10 +1,10 @@
 import React, { PropTypes } from 'react';
 
-function Header({ gc, am, appLanguage, refresh }) {
+function Header({ gc, am, language, refresh }) {
   return (
-    <div className={appLanguage === 'am' ? 'showtime-header -am-' : 'showtime-header'}>
+    <div className={language === 'am' ? 'showtime-header -am-' : 'showtime-header'}>
       <div className="header-date">
-        { appLanguage === 'en' ? gc : am }
+        { language === 'en' ? gc : am }
       </div>
 
       <i className="icon-refresh" onClick={refresh} />
@@ -15,14 +15,14 @@ function Header({ gc, am, appLanguage, refresh }) {
 Header.propTypes = {
   gc: PropTypes.string,
   am: PropTypes.string,
-  appLanguage: PropTypes.string,
+  language: PropTypes.string,
   refresh: PropTypes.func.isRequired,
 };
 
 Header.defaultProps = {
   gc: 'Tuesday October 26, 2017',
   am: 'ማክሰኞ',
-  appLanguage: 'am',
+  language: 'am',
 };
 
 module.exports = Header;
