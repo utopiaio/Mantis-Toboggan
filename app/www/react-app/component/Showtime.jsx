0460566e292ab8374ba13a4af7a06acddf8da23d
Eyassu <utopiaio@yahoo.com>
2016-10-27 07:22:31 +0300

    redux on showtime
    - init (lf cost is around ~200ms, maybe do stuff before splash screen is done 🤔)
    - all dispatchers (except poster) are 👍

--- app/www/react-app/component/Showtime.jsx
+++ app/www/react-app/component/Showtime.jsx
@@ -2,29 +2,47 @@
 /* eslint no-console: 0 */
 
 import React, { Component } from 'react';
+
+import store from '../redux/store';
+import { showtime, language, theme, poster } from '../redux/action/showtime';
+
 import Header from './Header.jsx';
 import Menu from './Menu.jsx';
 
 class Showtime extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      loading: false,
+      showtime: Object.create(null),
+      theme: 'night',
+      language: 'en',
+    };
   }
 
   componentDidMount() {
+    this.unsubscribe = store.subscribe(() => {
+      this.setState(store.getState());
+      console.log(this.state);
+    });
+
+    showtime();
+    // language('am');
+    // theme('light');
+
     // document.addEventListener('deviceready', () => {
     //   console.log('device ready...');
     // }, false);
   }
 
-  refresh() {
-    console.log('errrlow');
+  componentWillUnmount() {
+    this.unsubscribe();
   }
 
   render() {
     return (
       <div>
-        <Header refresh={this.refresh} />
+        <Header refresh={showtime} />
 
         <div className="showtime-view">
           { this.props.children }
