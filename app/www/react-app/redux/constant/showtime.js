6f3f51d4ec17b282aa27651f0fc232cbbc01165f
Moe Szyslak <moe.duffdude@gmail.com>
2016-10-27 07:20:05 +0300

    Black Republican

---
+++ app/www/react-app/redux/constant/showtime.js
@@ -0,0 +1,29 @@
+const SHOWTIME = 'SHOWTIME';
+const LANGUAGE = 'LANGUAGE';
+const THEME = 'THEME';
+const LOADING = 'LOADING';
+const POSTER = 'POSTER';
+
+// Localforage
+const LF_SHOWTIME = 'SHOWTIME';
+const LF_LANGUAGE = 'LANGUAGE';
+const LF_THEME = 'THEME';
+const LF_POSTER = 'POSTER';
+
+// API
+const API = 'http://s3.io/showtime/ednamall.json';
+
+module.exports = {
+  SHOWTIME,
+  LANGUAGE,
+  THEME,
+  POSTER,
+  LOADING,
+
+  LF_SHOWTIME,
+  LF_LANGUAGE,
+  LF_THEME,
+  LF_POSTER,
+
+  API,
+};
