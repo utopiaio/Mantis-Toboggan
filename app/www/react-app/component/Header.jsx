9aaf156f1a412403700471858f6f7ef8204490e5
Eyassu <utopiaio@yahoo.com>
2016-10-28 06:45:33 +0300

    header done with all the props and styles...i think

--- app/www/react-app/component/Header.jsx
+++ app/www/react-app/component/Header.jsx
@@ -2,11 +2,11 @@ import React, { PropTypes } from 'react';
 
 import Spinner from './Spinner.jsx';
 
-function Header({ gc, am, language, refresh, loading }) {
+function Header({ language, refresh, loading, ec, gc }) {
   return (
     <div className={language === 'am' ? 'showtime-header -am-' : 'showtime-header'}>
       <div className="header-date">
-        { language === 'en' ? gc : am }
+        { language === 'en' ? gc : ec }
       </div>
 
       <Spinner refresh={refresh} loading={loading} />
@@ -15,18 +15,18 @@ function Header({ gc, am, language, refresh, loading }) {
 }
 
 Header.propTypes = {
-  gc: PropTypes.string,
-  am: PropTypes.string,
   language: PropTypes.string,
   refresh: PropTypes.func.isRequired,
   loading: PropTypes.bool,
+  gc: PropTypes.string,
+  ec: PropTypes.string,
 };
 
 Header.defaultProps = {
-  gc: 'Tuesday October 26, 2017',
-  am: 'ማክሰኞ',
   language: 'am',
   loading: false,
+  gc: '1991-9-8',
+  ec: '1991-9-8',
 };
 
 module.exports = Header;
