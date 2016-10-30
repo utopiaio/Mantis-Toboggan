6b0e4f5b588313029a1589e416fcdfe47327be38
Moe Szyslak <moe.duffdude@gmail.com>
2016-10-30 07:15:55 +0300

    settings now a react component with state

--- app/www/react-app/component/Setting.jsx
+++ app/www/react-app/component/Setting.jsx
@@ -1,11 +1,47 @@
-import React, { PropTypes } from 'react';
+import React, { Component } from 'react';
 
-function Setting({  }) {
-  return (<div>Setting</div>);
+import store from '../redux/store';
+import { language, theme } from '../redux/action/showtime';
+
+class Setting extends Component {
+  constructor(props) {
+    const state = store.getState();
+
+    super(props);
+    this.state = {
+      language: state.language,
+      theme: state.theme,
+    };
+  }
+
+  componentDidMount() {
+    this.unsubscribe = store.subscribe(() => {
+      const state = store.getState();
+      this.setState({ language: state.language, theme: state.theme });
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribe();
+  }
+
+  changeLanguage() {
+    language(this.state.language === 'en' ? 'am' : 'en');
+  }
+
+  changeTheme() {
+    theme(this.state.theme === 'night' ? 'light' : 'night');
+  }
+
+  render() {
+    return (
+      <div>
+        <span>Setting</span>
+        <button onClick={() => this.changeLanguage()}>change language</button>
+        <button onClick={() => this.changeTheme()}>change theme</button>
+      </div>
+    );
+  }
 }
-
-Setting.propTypes = {};
-
-Setting.defaultProps = {};
 
 module.exports = Setting;
