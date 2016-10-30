d924949795fcf73c15f2db4a3600c3dd25e3d833
Moe Szyslak <moe.duffdude@gmail.com>
2016-10-30 08:14:01 +0300

    single entry

--- app/www/react-app/index.jsx
+++ app/www/react-app/index.jsx
@@ -8,8 +8,6 @@ import routes from './config/routes.jsx';
 import 'normalize.css/normalize.css';
 import './static/font/iconmoon/style.css';
 
-import './less/video.less';
-import './less/spinner.less';
 import './less/app.less';
 
 render(routes, document.getElementById('app'));
