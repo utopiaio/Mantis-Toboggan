d129a5217b2236949265b0e13adb77e493bd35d5
Moe Szyslak <moe.duffdude@gmail.com>
2016-10-31 10:25:30 +0300

    playing with rick...

--- app/www/react-app/component/Rick.jsx
+++ app/www/react-app/component/Rick.jsx
@@ -1,7 +1,46 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
 
-function Rick({  }, context) {
-  return (<div>Rick!</div>);
+import store from '../redux/store';
+
+class Rick extends Component {
+  constructor(props) {
+    super(props);
+
+    const state = store.getState();
+    this.state = {
+      poster: state.poster,
+      show: state.showtime.show,
+    };
+  }
+
+  componentDidMount() {
+    this.unsubscribe = store.subscribe(() => {
+      const state = store.getState();
+      this.setState({
+        poster: state.poster,
+        show: state.showtime.show,
+      });
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribe();
+  }
+
+  render() {
+    return (
+      <div className="view-rick">
+        { this.state.show.c1.map((movie, index) =>
+          <img
+            className="img-poster"
+            key={index}
+            src={this.state.poster[movie.title] || movie.poster}
+            alt={movie.title}
+          />)
+        }
+      </div>
+    );
+  }
 }
 
 Rick.propTypes = {};
