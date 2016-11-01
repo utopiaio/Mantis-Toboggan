12545953f335bff2b627cf8e0348201c5cbdce8a
Moe Szyslak <moe.duffdude@gmail.com>
2016-11-01 04:30:36 +0300

    one you've been all waiting for
    - this removes the ? : repeat 😅

---
+++ app/www/react-app/util/amClass.js
@@ -0,0 +1,9 @@
+/* eslint no-confusing-arrow: 0 */
+
+/**
+ * a util to be used to add `-am-` or not
+ * @param  {String} language
+ * @return {String}
+ */
+
+module.exports = language => language === 'am' ? '-am-' : '';
