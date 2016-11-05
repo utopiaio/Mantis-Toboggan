a9d8a188c962317ddd2db7610b4f8dd8d6aff4e7
Eyassu <utopiaio@yahoo.com>
2016-11-05 11:57:14 +0300

    magic 🎩
    - movie detail now takes header with it, gives it back when leaving
    - movie detail now takes the status bar too

--- app/www/react-app/component/Movie.jsx
+++ app/www/react-app/component/Movie.jsx
@@ -1,3 +1,4 @@
+/* global window */
 /* eslint no-console: 0 */
 
 import React, { Component, PropTypes } from 'react';
@@ -9,8 +10,44 @@ class Movie extends Component {
     this.state = {};
   }
 
+  componentWillMount() {
+    const header = window.document.querySelector('.showtime-header');
+    if (header !== null) {
+      header.style.flex = '0 0 0';
+    }
+
+    const showtimeView = window.document.querySelector('.showtime-view');
+    if (showtimeView) {
+      showtimeView.style.overflowY = 'hidden';
+    }
+
+    setTimeout(() => {
+      if (window.StatusBar !== undefined) {
+        window.StatusBar.hide();
+      }
+    }, 250);
+  }
+
   componentDidMount() {
     console.log('Movie CDM', this.props.params);
+  }
+
+  componentWillUnmount() {
+    const header = window.document.querySelector('.showtime-header');
+    if (header !== null) {
+      header.style.flex = '0 0 64px';
+    }
+
+    const showtimeView = window.document.querySelector('.showtime-view');
+    if (showtimeView) {
+      showtimeView.style.overflowY = 'scroll';
+    }
+
+    setTimeout(() => {
+      if (window.StatusBar !== undefined) {
+        window.StatusBar.show();
+      }
+    }, 250);
   }
 
   render() {
