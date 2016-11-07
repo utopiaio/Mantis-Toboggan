72d7db89c1391b126c8590f0e4b7fd9f48050988
Eyassu <utopiaio@yahoo.com>
2016-11-07 06:40:20 +0300

    class name change
    - lets keep it valid

--- app/www/react-app/util/amClass.js
+++ app/www/react-app/util/amClass.js
@@ -1,9 +1,9 @@
 /* eslint no-confusing-arrow: 0 */
 
 /**
- * a util to be used to add `-am-` or not
+ * a util to be used to add `_am_` or not
  * @param  {String} language
  * @return {String}
  */
 
-module.exports = language => language === 'am' ? '-am-' : '';
+module.exports = language => language === 'am' ? '_am_' : '';
