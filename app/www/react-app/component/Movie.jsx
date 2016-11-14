28831b02a6c102b22e4ec8968c443f13631d8bc6
Eyassu <utopiaio@yahoo.com>
2016-11-14 18:29:58 +0300

    411 and video
    - this is a big one
    
    🤔 abusing the ternary operator and leaving with span...

--- app/www/react-app/component/Movie.jsx
+++ app/www/react-app/component/Movie.jsx
@@ -86,7 +86,59 @@ class Movie extends Component {
         <div className="movie-411">
           <h2 className={`light-font-weight movie-title ${containsFidel(this.state.movie.title) ? '_am_' : ''}`}>{ this.state.movie.title }</h2>
           <p className="movie-showtime _am_">{this.state.movie.time}</p>
-          <h3 className={`movie-information light-font-weight ${amClass(this.state.language)}`}>{i18n[this.state.language].INFORMATION}</h3>
+          {
+            this.state.movie.video ? <div>
+              <div className={`video-label ${amClass(this.state.language)}>{i18n[this.state.language].VIDEO}`}>
+                {i18n[this.state.language].VIDEO}
+              </div>
+
+              <div className="video-container">
+                <iframe
+                  src={this.state.movie.video}
+                  frameBorder="0"
+                  allowFullScreen
+                />
+              </div>
+            </div> : <span />
+          }
+          {
+            this.state.movie.omdb ? <h3 className={`movie-information ${amClass(this.state.language)}`}>
+              <table>
+                <caption>{i18n[this.state.language].INFORMATION}</caption>
+                <tbody>
+                  <tr>
+                    <td>Rated</td>
+                    <td>{ this.state.movie.omdb.Rated }</td>
+                  </tr>
+
+                  <tr>
+                    <td>Released</td>
+                    <td>{ this.state.movie.omdb.Released }</td>
+                  </tr>
+
+                  <tr>
+                    <td>Genre</td>
+                    <td>{ this.state.movie.omdb.Genre }</td>
+                  </tr>
+
+                  <tr>
+                    <td>Director</td>
+                    <td>{ this.state.movie.omdb.Director }</td>
+                  </tr>
+
+                  <tr>
+                    <td>Cast</td>
+                    <td>{ this.state.movie.omdb.Actors }</td>
+                  </tr>
+
+                  <tr>
+                    <td>Run Time</td>
+                    <td>{ this.state.movie.omdb.Runtime }</td>
+                  </tr>
+                </tbody>
+              </table>
+            </h3> : <span />
+          }
         </div>
       </div>
     );
