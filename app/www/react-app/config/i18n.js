d623657b940665efb6d27d089be19f3a75a219f6
Moe Szyslak <moe.duffdude@gmail.com>
2016-11-15 05:54:20 +0300

    send feedback button
    TODO:
    - test mailto link on device

--- app/www/react-app/config/i18n.js
+++ app/www/react-app/config/i18n.js
@@ -7,7 +7,9 @@ module.exports = {
     INFORMATION: 'መረጃ',
     CHANGE_LANGUAGE: 'ቋንቋ ቀይር',
     CHANGE_THEME: 'ገጽታ ቀይር',
+    CONTACT: 'Send Feedback',
   },
+
   en: {
     MOVIE: 'Movie',
     MOVIES: 'Movies',
@@ -16,5 +18,6 @@ module.exports = {
     INFORMATION: 'Information',
     CHANGE_LANGUAGE: 'Change Language',
     CHANGE_THEME: 'Change Theme',
+    CONTACT: 'Send Feedback',
   },
 };
