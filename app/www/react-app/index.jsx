1ca0be77ceeea964147f9dba433e036363b2dfb8
Moe Szyslak <moe.duffdude@gmail.com>
2016-11-17 06:57:46 +0300

    animate.css class picked out
    - _fixed_ minor flicking on animate up
    - `deviceready` boots then react

--- app/www/react-app/index.jsx
+++ app/www/react-app/index.jsx
@@ -6,13 +6,11 @@ import { render } from 'react-dom';
 import routes from './config/routes.jsx';
 
 import 'normalize.css/normalize.css';
-import 'animate.css';
 import './static/font/iconmoon/style.css';
-
 import './less/app.less';
 
 // Turn on before build
-// window.document.addEventListener('deviceready', () => {
-render(routes, document.getElementById('app'));
-// }, false);
+window.document.addEventListener('deviceready', () => {
+  render(routes, document.getElementById('app'));
+}, false);
 
