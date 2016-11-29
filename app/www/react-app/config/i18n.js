7abeb2751266b809c879bbc0186a632dc9a211ef
Moe Szyslak <moe.duffdude@gmail.com>
2016-11-29 06:55:43 +0300

    close button at the end of video

--- app/www/react-app/config/i18n.js
+++ app/www/react-app/config/i18n.js
@@ -8,7 +8,7 @@ module.exports = {
     CHANGE_LANGUAGE: 'ቋንቋ ቀይር',
     CHANGE_THEME: 'ገጽታ ቀይር',
     CONTACT: 'አስተያየት ይላኩ',
-    SHOW_ME: '<አሳየኝ />',
+    CLOSE: 'ዝጋ',
   },
 
   en: {
@@ -20,6 +20,6 @@ module.exports = {
     CHANGE_LANGUAGE: 'Change Language',
     CHANGE_THEME: 'Change Theme',
     CONTACT: 'Send Feedback',
-    SHOW_ME: '<ShowMe />',
+    CLOSE: 'Close',
   },
 };
