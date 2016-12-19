bfa013dcebb6b3d4decd18b67edb8f2cec8e7761
Moe Szyslak <moe.duffdude@gmail.com>
2016-12-19 12:22:50 +0300

    `app` moved to separate repository
    - project name change

--- app/www/react-app/config/history.js
+++
@@ -1,6 +0,0 @@
-import { useRouterHistory } from 'react-router';
-import createHashHistory from 'history/lib/createHashHistory';
-
-const history = useRouterHistory(createHashHistory)();
-
-module.exports = history;
