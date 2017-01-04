7efdc68aa1d6c0f5705b2723cb76b93683ec5a8c
Moe Szyslak <moe.duffdude@gmail.com>
2017-01-04 03:27:52 +0300

    more fine grained CORS options
    - limited allowed methods

--- app.js
+++ app.js
@@ -48,7 +48,15 @@ moedoo.query(`
     app.use((req, res, next) => {
       res.header('Access-Control-Allow-Origin', '*');
       res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-      next();
+      res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+      res.header('Access-Control-Allow-Credentials', 'true');
+      res.header('Access-Control-Max-Age', '86400');
+
+      if (req.method === 'OPTIONS') {
+        res.status(200).end();
+      } else {
+        next();
+      }
     });
     app.use(compression());
 
