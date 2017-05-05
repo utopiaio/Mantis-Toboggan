8bc7b2679bcae03953f3c8e7eac7d6877ce6a466
Eyassu <utopiaio@yahoo.com>
2017-05-05 11:39:36 +0300

    human typos being resolved
    TODO:
    - Telegram Bot for the typo

---
+++ humans/typo.js
@@ -0,0 +1,23 @@
+/**
+ * This object export is used to fix title typos from Ethio-Yello pages
+ * so Lethal Weapon 5 can do its thing
+ *
+ * Structure:
+ * { [Typo Movie]: "Correct Movie Title" }
+ */
+
+const typos = {
+  'Guardians of the Galaxy2': 'Guardians of the Galaxy Vol. 2',
+};
+
+module.exports = (title) => {
+  const keys = Object.keys(typos); // DJ Khaled
+  const smallKeys = keys.map(key => key.toLowerCase());
+  const movieIndex = smallKeys.indexOf(title.toLowerCase());
+
+  if (movieIndex === -1) {
+    return title;
+  }
+
+  return typos[keys[movieIndex]];
+};
