bc146347e204584a5c88182014a2c7a850025122
Utopia <utopiaio@yahoo.com>
2017-09-25 10:11:28 +0300

    indentation

--- app.js
+++ app.js
@@ -32,42 +32,42 @@ moedoo.query(`
     CONSTRAINT poster_pk PRIMARY KEY (url)
   );
 
-  -- the following three can be one
+  -- the following two can be one
   DELETE FROM omdb WHERE created_at < '${moment().subtract(5, 'days').format('YYYY-MM-DD')}';
   DELETE FROM poster WHERE created_at < '${moment().subtract(30, 'days').format('YYYY-MM-DD')}';`).then(() => {
-    const app = express();
-    app.set('port', process.env.PORT || config.APP_PORT);
-    app.use((req, res, next) => {
-      res.header('Access-Control-Allow-Origin', '*');
-      res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-      res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
-      res.header('Access-Control-Allow-Credentials', 'true');
-      res.header('Access-Control-Max-Age', '86400');
+  const app = express();
+  app.set('port', process.env.PORT || config.APP_PORT);
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Max-Age', '86400');
 
-      if (req.method === 'OPTIONS') {
-        res.status(200).end();
-        return;
-      }
+    if (req.method === 'OPTIONS') {
+      res.status(200).end();
+      return;
+    }
 
-      next();
-    });
-    app.use(compression());
-
-    // API...
-    app.get('/api', api(moedoo));
-
-    // Poster...
-    app.get('/poster', poster(moedoo));
-
-    // fall-back handler...
-    app.all('*', (request, response) => {
-      response.status(400).end();
-    });
-
-    const server = http.createServer(app); // creating server which express will piggy back on
-    server.listen(app.get('port'), config.APP_HOST, () => {
-      console.log(`Server running on ${config.APP_HOST}:${process.env.PORT || config.APP_PORT}...`);
-    });
-  }, (err) => {
-    console.error(err);
+    next();
   });
+  app.use(compression());
+
+  // API...
+  app.get('/api', api(moedoo));
+
+  // Poster...
+  app.get('/poster', poster(moedoo));
+
+  // fall-back handler...
+  app.all('*', (request, response) => {
+    response.status(400).end();
+  });
+
+  const server = http.createServer(app); // creating server which express will piggy back on
+  server.listen(app.get('port'), config.APP_HOST, () => {
+    console.log(`Server running on ${config.APP_HOST}:${process.env.PORT || config.APP_PORT}...`);
+  });
+}, (err) => {
+  console.error(err);
+});
