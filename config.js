415553b702f2f24d45db9756e9a465796004f3d2
Utopia <utopiaio@yahoo.com>
2017-09-25 11:56:48 +0300

    based
    - no need for scraping ET Yellowpages - they're DED

--- config.js
+++ config.js
@@ -10,8 +10,6 @@ const DB_PORT = 5432;
 const DB_NAME = 'showtime';
 const APP_HOST = '0.0.0.0';
 const APP_PORT = '8000';
-const URL_BASE = 'https://www.etyellowpages.com/';
-const URL = 'https://www.etyellowpages.com/Movie-Showtimes/';
 
 module.exports = {
   DB_HOST,
@@ -21,6 +19,4 @@ module.exports = {
   DB_NAME,
   APP_HOST,
   APP_PORT,
-  URL_BASE,
-  URL,
 };
---
+++ schedule.js
@@ -0,0 +1,102 @@
+// DD-MM-YYYY => { schedule }
+
+module.exports = {
+  '2017-09-25': {
+    c1: [
+      {
+        title: 'Kingsman: The Golden Circle',
+        posterURL: 'https://www.etyellowpages.com/images/cinema/NoPoster.jpg',
+        showtime: {
+          et: '08:00 ቀን',
+          gc: '02:00 PM',
+        },
+        detail: null,
+      },
+      {
+        title: 'IT',
+        posterURL: 'https://www.etyellowpages.com/images/cinema/NoPoster.jpg',
+        showtime: {
+          et: '10:00 ቀን',
+          gc: '04:00 PM',
+        },
+        detail: null,
+      },
+      {
+        title: 'IT',
+        posterURL: 'https://www.etyellowpages.com/images/cinema/NoPoster.jpg',
+        showtime: {
+          et: '12:45 ምሽት',
+          gc: '06:45 PM',
+        },
+        detail: null,
+      },
+      {
+        title: 'IT',
+        posterURL: 'https://www.etyellowpages.com/images/cinema/NoPoster.jpg',
+        showtime: {
+          et: '03:15 ምሽት',
+          gc: '09:15 PM',
+        },
+        detail: null,
+      },
+    ],
+    c2: [
+      {
+        title: 'Kingsman: The Golden Circle',
+        posterURL: 'https://www.etyellowpages.com/images/cinema/NoPoster.jpg',
+        showtime: {
+          et: '07:00 ቀን',
+          gc: '01:00 PM',
+        },
+        detail: null,
+      },
+      {
+        title: 'Kingsman: The Golden Circle',
+        posterURL: 'https://www.etyellowpages.com/images/cinema/NoPoster.jpg',
+        showtime: {
+          et: '10:00 ቀን',
+          gc: '04:00 PM',
+        },
+        detail: null,
+      },
+      {
+        title: 'Lefkre Sil',
+        posterURL: 'https://www.etyellowpages.com/images/cinema/NoPoster.jpg',
+        showtime: {
+          et: '12:45 ምሽት',
+          gc: '06:45 PM',
+        },
+        detail: null,
+      },
+      {
+        title: 'Lefkre Sil',
+        posterURL: 'https://www.etyellowpages.com/images/cinema/NoPoster.jpg',
+        showtime: {
+          et: '03:15 ምሽት',
+          gc: '09:15 PM',
+        },
+        detail: null,
+      },
+    ],
+    c3: [
+      {
+        title: 'Kingsman: The Golden Circle',
+        posterURL: 'https://www.etyellowpages.com/images/cinema/NoPoster.jpg',
+        showtime: {
+          et: '12:45 ምሽት',
+          gc: '06:45 PM',
+        },
+        detail: null,
+      },
+      {
+        title: 'Kingsman: The Golden Circle',
+        posterURL: 'https://www.etyellowpages.com/images/cinema/NoPoster.jpg',
+        showtime: {
+          et: '03:15 ምሽት',
+          gc: '09:15 PM',
+        },
+        detail: null,
+      },
+    ],
+  },
+};
